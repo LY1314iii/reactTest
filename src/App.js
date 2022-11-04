733b662b5976f52c6049efbd59662c5d9b3d30ef
LY1314iii <943447716@qq.com>
2022-11-04 13:34:23 +0800

    提交

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import { createContext, useContext } from 'react'
+// 创建Context对象
+const C = createContext()
+
+function Foo() {  
+    return <div>Foo <Bar/></div>
 }
 
-export default App;
+function Bar() {  
+    // 底层组件通过useContext函数获取数据  
+    
+    return <div>Bar {useContext(C)}</div>
+}
+
+function App() {  
+    return (    
+        // 顶层组件通过Provider 提供数据    
+        <C.Provider value={'this is name'}>     
+            <div><Foo/></div>    
+        </C.Provider>  
+    )
+}
+
+export default App
